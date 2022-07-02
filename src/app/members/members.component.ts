eaf95703379512ea31663043df42306aae96301d
tkkj0017 <tttvvvaken@gmail.com>
2022-07-02 16:57:39 +0900

    MemberComponentの追加

---
+++ src/app/members/members.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../member';
+import { MEMBERS } from '../mock-members';
+
+@Component({
+  selector: 'app-members',
+  templateUrl: './members.component.html',
+  styleUrls: ['./members.component.css']
+})
+export class MembersComponent implements OnInit {
+
+  members = MEMBERS;
+  member: Member = {
+    id: 1,
+    name: 'kojima'
+  };
+  selectedMember: Member;
+
+  constructor() { }
+
+  // ライフサイクルメソッド
+  // コンポネントが初期化される度に実行されるメソッド
+  ngOnInit(): void {
+  }
+
+  onSelect(member: Member): void {
+    this.selectedMember = member;
+  }
+}
